06f13d4067ced1d64835ab07c190973523257a33
raya.loly <raya.loly@salt.co.id>
2022-05-09 10:16:23 +0800

    new token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,9 @@ export class AppComponent {
   title = 'angular-oz';
 
   openCamera() {
-    console.log('button clicked');
-
     OzLiveness.open({
       lang: 'en',
-      token: '84b10f0fe3151418daba2efe017cc97fb2a290cfe1952b545c1f9bb5163acba34112890d6c293b55d96a3dcbc2b286828bcf5636c534985686697df3b6fd21cd',
+      token: '2e9f03ac94f79a178a9615144833b2298c4f3541abd91a94808b79b78df5b342ad3a66849347e541c376394fdc7d4c27d5aed234c707b8292c998c3ea2a3681b',
       on_complete: function () {
         alert('Human');
       }
