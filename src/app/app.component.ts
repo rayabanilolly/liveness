2725cad038730e3d916e816419f7c7e7e54dcfb2
rayabanilolly <raya.secondary@gmail.com>
2022-05-13 18:10:36 +0800

    log result when complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,8 @@ export class AppComponent {
     OzLiveness.open({
       lang: 'en',
       token: '5f6dd74ec937171541b992b7f6d21dff1cbdc9a516bb511f7152fbdb751c590277e5cd99fc1980168675fb56c2d586f250d7e0ab276bc7a8dec86d0e5b5b176b',
-      on_complete: function () {
-        alert('Human');
+      on_complete: function (result: any) {
+        console.log(result)
       }
     });
   }
